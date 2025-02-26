d6341aeac901dfdf4801b71d4dbad143df9fac2e
daffarahman <69158099+daffarahman@users.noreply.github.com>
2025-02-26 20:53:20 +0700

    updated main tugas1

--- sem2/sda/tugas1/c/main.c
+++ sem2/sda/tugas1/c/main.c
@@ -12,12 +12,6 @@
 
 #include "math_operations.h"
 
-// definitions for bitwise operation
-#define ADD 1
-#define SUBTRACT 2
-#define MULTIPLY 3
-#define DIVIDE 4
-
 // fungsi clear screen yang dapat work di windows dan non-windows (mac, linux)
 void clear_screen()
 {
@@ -26,6 +20,26 @@ void clear_screen()
 #else
     system("clear");
 #endif
+}
+
+void print_add(int a, int b)
+{
+    printf("%d + %d = %d\n", a, b, add(a, b));
+}
+
+void print_subtract(int a, int b)
+{
+    printf("%d + %d = %d\n", a, b, subtract(a, b));
+}
+
+void print_multiply(int a, int b)
+{
+    printf("%d + %d = %d\n", a, b, multiply(a, b));
+}
+
+void print_divide(int a, int b)
+{
+    printf("%d + %d = %.2f\n", a, b, divide(a, b));
 }
 
 int main()
@@ -37,20 +51,50 @@ int main()
     char *str_input = (char *)malloc(sizeof(char) * 255);
     int int_input = 0;
 
-    for (;;)
+    clear_screen();
+
+    printf("Welcome to ZuperSymple Calculator\n\n");
+
+    printf("enter a: ");
+    scanf("%d", &a);
+
+    printf("enter b: ");
+    scanf("%d", &b);
+
+    printf("\nWhat operation you want to do?\n"
+           " 1. Add (+)\n"
+           " 2. Subtract (-)\n"
+           " 3. Multiply (*)\n"
+           " 4. Divide (/)\n"
+           " 5. All\n\n");
+
+    printf("Selection: ");
+    while (!(int_input > 0 && int_input <= 5))
     {
-        printf("Welcome to ZuperSymple Calculator\n\n");
+        fgets(str_input, 255, stdin);
+        int_input = atoi(str_input);
+    }
 
-        printf("enter a: ");
-        scanf("%d", &a);
+    printf("\nResult:\n");
 
-        printf("enter b: ");
-        scanf("%d", &b);
-
-        printf("What operation you want to do?\n"
-               " 1. Add\n"
-               " 2. Subtract\n"
-               " 3. Multiply\n"
-               " 4. Divide\n");
+    switch (int_input)
+    {
+    case 5:
+    case 1:
+        print_add(a, b);
+        if (int_input != 5)
+            break;
+    case 2:
+        print_subtract(a, b);
+        if (int_input != 5)
+            break;
+    case 3:
+        print_multiply(a, b);
+        if (int_input != 5)
+            break;
+    case 4:
+        print_divide(a, b);
+        if (int_input != 5)
+            break;
     }
 }
